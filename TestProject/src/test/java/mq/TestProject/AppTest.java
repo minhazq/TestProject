324a7f33effd3366f065b5f2eaa69a9deb9e2ddd
minhazq <minhazq@gmail.com>
2017-03-01 17:56:39 -0500

    Selenium

--- TestProject/src/test/java/mq/TestProject/AppTest.java
+++ TestProject/src/test/java/mq/TestProject/AppTest.java
@@ -1,38 +1,15 @@
 package mq.TestProject;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+    
+	
+	@Test
+	public void m1(){
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.bn.com");
+	}
 }
